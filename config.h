c967846540966cb396f1c57c7bcaf5ad6ae61f53
jonay2000 <jonabent@gmail.com>
2020-04-22 00:19:01 +0200

    added sed

--- config.h
+++ config.h
@@ -1,13 +1,12 @@
 /* user and group to drop privileges to */
-static const char *user  = "jonathan";
-static const char *group = "jonathan";
+static const char *user  = "nobody";
+static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#32a852",     /* after initialization */
+	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
-	[CAPS] = "red",         /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 0;
+static const int failonclear = 1;
